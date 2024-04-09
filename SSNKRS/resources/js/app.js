381715ef02a21668139ca566c7e1d9de731270a5
marcportet <marc_portetsalguero@iescarlesvallbona.cat>
2024-04-09 17:09:49 +0200

    prueba git 2

--- SSNKRS/resources/js/app.js
+++ SSNKRS/resources/js/app.js
@@ -15,7 +15,7 @@ import { createApp } from 'vue';
 
 const app = createApp({});
 
-import ExampleComponent from './components/ExampleComponent.vue';
+// import ExampleComponent from './components/ExampleComponent.vue';
 // app.component('example-component', ExampleComponent);
 
 /**
