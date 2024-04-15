cefc2f23947991f2f0925a5d5f31fa188a0c1b8e
marcportet <marc_portetsalguero@iescarlesvallbona.cat>
2024-04-15 19:29:53 +0200

    Cors Added to API

--- Sneakers_API/app.js
+++ Sneakers_API/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const db = require("./db");
 const fs = require("fs");
 const path = require("path");
+const cors = require("cors"); 
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.listen(PORT, () => {
   // Iniciar el servidor
