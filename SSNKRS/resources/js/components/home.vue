c4b2edbad41750312e83390dd0b8fae9cc12ec82
marcportet <marc_portetsalguero@iescarlesvallbona.cat>
2024-04-15 18:30:35 +0200

    Home basico sin datos

--- SSNKRS/resources/js/components/home.vue
+++ SSNKRS/resources/js/components/home.vue
@@ -1,42 +1,273 @@
-<template>        
-<div class="home">
-<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img :src="'img/imgCarrusel1.jpg'" class="d-block w-100" alt="...">
+<template>
+    <div class="home bg-white">
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img :src="'img/imgCarrusel1.jpg'" class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                    <img src="img/imgCarrusel2.jpg" class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                    <img src="img/imgCarrusel3.jpg" class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                    <img src="img/imgCarrusel4.jpg" class="d-block w-100" alt="..." />
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
-        <div class="carousel-item">
-            <img src="img/imgCarrusel2.jpg" class="d-block w-100" alt="...">
+
+        <div class="pt-5 pb-3 px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+            <div class="titulo">
+                <span>Lo Mas Vendido</span>
+                <a class="vermas" href="#">
+                    ver mas
+                    <svg class="h-8" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor"
+                        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"></path>
+                    </svg>
+                </a>
+            </div>
+            <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                <div v-for="producto in productos_masvendidos" :key="producto.id" class="group relative">
+                    <div
+                        class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                        <img :src="producto.imageSrc" :alt="producto.imageAlt"
+                            class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+                    </div>
+                    <div class="mt-4 flex justify-between">
+                        <div>
+                            <h3 class="text-base text-gray-700">
+                                <a :href="producto.href">
+                                    <span aria-hidden="true" class="absolute inset-0" />
+                                    {{ producto.name }}
+                                </a>
+                            </h3>
+                            <p class="mt-1 text-base text-gray-500">{{ producto.color }}</p>
+                        </div>
+                        <p class="text-base font-medium text-gray-900">{{ producto.price }}</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="carousel-item">
-            <img src="img/imgCarrusel3.jpg" class="d-block w-100" alt="...">
+
+
+        <div class="pt-5 pb-3 px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+            <div class="titulo">
+                <span>Nuevas Sneakers</span>
+                <a class="vermas" href="#">
+                    ver mas
+                    <svg class="h-8" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor"
+                        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"></path>
+                    </svg>
+                </a>
+            </div>
+            <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                <div v-for="producto in productos_nuevos" :key="producto.id" class="group relative">
+                    <div
+                        class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                        <img :src="producto.imageSrc" :alt="producto.imageAlt"
+                            class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+                    </div>
+                    <div class="mt-4 flex justify-between">
+                        <div>
+                            <h3 class="text-base text-gray-700">
+                                <a :href="producto.href">
+                                    <span aria-hidden="true" class="absolute inset-0" />
+                                    {{ producto.name }}
+                                </a>
+                            </h3>
+                            <p class="mt-1 text-base text-gray-500">{{ producto.color }}</p>
+                        </div>
+                        <p class="text-base font-medium text-gray-900">{{ producto.price }}</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="carousel-item">
-            <img src="img/imgCarrusel4.jpg" class="d-block w-100" alt="...">
+
+
+        <div class="pt-5 pb-3 px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+            <div class="titulo">
+                <span>Próximos Lazamientos</span>
+                <a class="vermas" href="#">
+                    ver mas
+                    <svg class="h-8" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor"
+                        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"></path>
+                    </svg>
+                </a>
+            </div>
+            <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+                <div v-for="producto in productos_proximos" :key="producto.id" class="group relative">
+                    <div
+                        class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-85">
+                        <img :src="producto.imageSrc" :alt="producto.imageAlt"
+                            class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="pt-5 pb-3 px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+            <div>
+                <span>As Seen On Instagram <br>
+                    Use #SkySNKRS and you could be featured.</span>
+            </div>
+            <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                <div class="group relative">
+                    <div
+                        class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-85">
+                        <img :src="'img/wastep.jpg'"
+                            alt="Front of men's Basic Tee in black."
+                            class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                    </div>
+                </div>
+                <div class="group relative">
+                    <div
+                        class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-85">
+                        <img :src="'img/marcp.jpg'"
+                            alt="Front of men's Basic Tee in black."
+                            class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                    </div>
+                </div>
+                <div class="group relative">
+                    <div
+                        class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-85">
+                        <img :src="'img/scrapw.jpg'"
+                            alt="Front of men's Basic Tee in black."
+                            class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                    </div>
+                </div>
+                <div class="group relative">
+                    <div
+                        class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-85">
+                        <img :src="'img/joelc.jpg'"
+                            alt="Front of men's Basic Tee in black."
+                            class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-</div>
-
-<div class="masVendidas">
-    <div class="titulo">
-        <span>Lo Mas Vendido</span>
-        <a class="vermas" href="#">
-    ver mas
-    <svg class="h-8" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"></path>
-    </svg>
-</a>
-
-        
-    </div>
-</div>
-</div>
 </template>
+
+<script setup>
+const productos_masvendidos = [
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: "$35",
+        color: "Black",
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: "$35",
+        color: "Black",
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: "$35",
+        color: "Black",
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: "$35",
+        color: "Black",
+    },
+    // More products...
+];
+const productos_nuevos = [
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: "$35",
+        color: "Black",
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: "$35",
+        color: "Black",
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: "$35",
+        color: "Black",
+    },
+    {
+        id: 1,
+        name: "Basic Tee",
+        href: "#",
+        imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: "$35",
+        color: "Black",
+    },
+];
+const productos_proximos = [
+    {
+        id: 1,
+        href: "#",
+        imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+        imageAlt: "Front of men's Basic Tee in black.",
+    },
+    {
+        id: 1,
+        href: "#",
+        imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+        imageAlt: "Front of men's Basic Tee in black.",
+    },
+    {
+        id: 1,
+        href: "#",
+        imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+        imageAlt: "Front of men's Basic Tee in black.",
+    },
+];
+</script>
