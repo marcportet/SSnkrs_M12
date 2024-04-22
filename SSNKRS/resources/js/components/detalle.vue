e0b44f385b251c89128f344bb392727254fa26be
marcportet <marc_portetsalguero@iescarlesvallbona.cat>
2024-04-22 19:05:11 +0200

    detalle info ajust

--- SSNKRS/resources/js/components/detalle.vue
+++ SSNKRS/resources/js/components/detalle.vue
@@ -1,41 +1,9 @@
 <template>
   <div class="bg-white">
     <div class="pt-6">
-      <nav aria-label="Breadcrumb">
-        <ol role="list" class="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-          <li v-for="breadcrumb in product.breadcrumbs" :key="breadcrumb.id">
-            <div class="flex items-center">
-              <a :href="breadcrumb.href" class="mr-2 text-sm font-medium text-gray-900">{{ breadcrumb.name }}</a>
-              <svg width="16" height="20" viewBox="0 0 16 20" fill="currentColor" aria-hidden="true"
-                class="h-5 w-4 text-gray-300">
-                <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
-              </svg>
-            </div>
-          </li>
-          <li class="text-sm">
-            <a :href="product.href" aria-current="page" class="font-medium text-gray-500 hover:text-gray-600">{{
-              product.name }}</a>
-          </li>
-        </ol>
-      </nav>
-
       <!-- Image gallery -->
-      <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
-        <div class="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
-          <img :src="product.images[0].src" :alt="product.images[0].alt"
-            class="h-full w-full object-cover object-center" />
-        </div>
-        <div class="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
-          <div class="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-            <img :src="product.images[1].src" :alt="product.images[1].alt"
-              class="h-full w-full object-cover object-center" />
-          </div>
-          <div class="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-            <img :src="product.images[2].src" :alt="product.images[2].alt"
-              class="h-full w-full object-cover object-center" />
-          </div>
-        </div>
-        <div class="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
+      <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-1 lg:gap-x-8 lg:px-8">
+        <div class="aspect-h-3 aspect-w-5 lg:aspect-h-3 lg:aspect-w-full sm:overflow-hidden sm:rounded-lg">
           <img :src="product.images[3].src" :alt="product.images[3].alt"
             class="h-full w-full object-cover object-center" />
         </div>
@@ -53,7 +21,7 @@
           <h2 class="sr-only">Product information</h2>
           <p class="text-3xl tracking-tight text-gray-900">{{ product.price }}</p>
 
-          <!-- Reviews -->
+          <!-- Reviews 
           <div class="mt-6">
             <h3 class="sr-only">Reviews</h3>
             <div class="flex items-center">
@@ -67,9 +35,10 @@
                 reviews.totalCount }} reviews</a>
             </div>
           </div>
+          -->
 
           <form class="mt-10">
-            <!-- Colors -->
+            <!-- Colors 
             <div>
               <h3 class="text-sm font-medium text-gray-900">Color</h3>
 
@@ -88,6 +57,7 @@
                 </div>
               </RadioGroup>
             </div>
+            -->
 
             <!-- Sizes -->
             <div class="mt-10">
@@ -127,7 +97,7 @@
         </div>
 
         <div class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
-          <!-- Description and details -->
+          <!-- Description and details 
           <div>
             <h3 class="sr-only">Description</h3>
 
@@ -155,6 +125,7 @@
               <p class="text-sm text-gray-600">{{ product.details }}</p>
             </div>
           </div>
+          -->
         </div>
       </div>
     </div>
