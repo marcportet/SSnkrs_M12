047951e8b9ae1a7898350a2e9f1b32b4f87b9ea6
JoelCuradoM <joel_curadomolinillo@iescarlesvallbona.cat>
2024-04-26 15:29:18 +0200

    arreglar tema boostrap i carrusel

--- SSNKRS/resources/views/app.blade.php
+++ SSNKRS/resources/views/app.blade.php
@@ -9,7 +9,9 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">        
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
