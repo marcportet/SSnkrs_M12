a833eab3c9c21177614c1a231c3d224c295f77be
marcportet <marc_portetsalguero@iescarlesvallbona.cat>
2024-04-26 15:09:54 +0200

    Cambio de estrorctura para comunicacion front y back

--- SSNKRS/app/Http/Controllers/HomeController.php
+++ SSNKRS/app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+
 
 class HomeController extends Controller
 {
@@ -13,7 +15,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('auth');
+        $this->middleware('auth');
     }
 
     /**
@@ -23,6 +25,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return Inertia::render('Views/home');
     }
 }
--- SSNKRS/resources/js/app.js
+++ SSNKRS/resources/js/app.js
@@ -1,101 +1,24 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 import './bootstrap';
+import '../css/app.css';
 
-import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-/**
- * Next, we will create a fresh Vue application instance. You may then begin
- * registering components with the application instance so they are ready
- * to use in your application's views. An example is included for you.
- */
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-import App from './components/app.vue';
-
-import login from './components/login.vue';
-import registro from './components/registro.vue';
-
-import home from './components/home.vue';
-import lanzamientos from './components/lanzamientos.vue';
-import contacto from './components/contacto.vue';
-import carrito from './components/carrito.vue';
-import catalogo from './components/catalogo.vue';
-
-
-import detalle from './components/detalle.vue';
-
-const ssnkrsapp = createApp(App);
-
-const routes = [
-    {
-        path: '/',
-        component: home
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
     },
-    {
-        path: '/login',
-        component: login
+    progress: {
+        color: '#4B5563',
     },
-    {
-        path: '/registro',
-        component: registro
-    },
-    {
-        path: '/lanzamientos',
-        component: lanzamientos
-    },
-    {
-        path: '/contacto',
-        component: contacto
-    },
-    {
-        path: '/carrito',
-        component: carrito
-    },
-    {
-        path: '/detalle/:id',
-        component: detalle
-    }, 
-    {
-        path: '/catalogo',
-        component: catalogo
-    },
-    {
-        path: '/catalogo/:brand',
-        component: catalogo
-    },
-]
+});
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
-
-// import ExampleComponent from './components/ExampleComponent.vue';
-// app.component('example-component', ExampleComponent);
-
-ssnkrsapp.use(router);
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
-//     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
-// });
-
-/**
- * Finally, we will attach the application instance to a HTML element with
- * an "id" attribute of "app". This element is included with the "auth"
- * scaffolding. Otherwise, you will need to add an element yourself.
- */
-
-ssnkrsapp.mount('#app');
